b9214aa157cbd74bed0889269f8bd09ed062649d
Jonathan Moody <jonathanmoody15@gmail.com>
2024-09-10 21:48:49 -0400

    index.html: 1. added "field" class to input elements excluding email input
    style.css:
    1. added styles for error class
    
    script.js:
    1. made click function to evaluate data in text field
    2. made function to determine if text in email field is a valid email

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const textField = document.getElementsByClassName("field");
+const emailField = document.getElementById("email");
+const button = document.getElementById("btn");
 
+button.addEventListener("click", function () {
+  for (i = 0; i < textField.length; i++) {
+    const field = textField[i];
+    if (field.value.length === 0) {
+      field.classList.add("error");
+      field.setAttribute("placeholder", "");
+      field.nextElementSibling.style.display = "block";
+    } else {
+      if (field.classList.contains("error")) {
+        field.classList.remove("error");
+        field.nextElementSibling.style.display = "none";
+      }
+    }
+  }
+
+  const emailValue = document.getElementById("email").value;
+
+  if (!validateEmail(emailValue)) {
+    emailField.classList.add("error");
+    emailField.setAttribute("placeholder", "");
+    emailField.nextElementSibling.style.display = "block";
+  } else {
+    if (emailField.classList.contains("error")) {
+      emailField.classList.remove("error");
+      emailField.nextElementSibling.style.display = "none";
+    }
+  }
+});
+
+function validateEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
